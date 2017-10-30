708bdf28124ff74947a2e674b489d21305820b25
Michael Angelo Autor <autormichaelangelo@gmail.com>
2017-10-30 13:11:27 +0800

    refactored code

--- app/page/sorting-page/sorting-page.component.ts
+++ app/page/sorting-page/sorting-page.component.ts
@@ -10,53 +10,38 @@ export class sortingPageComponent implements OnInit {
 
     sortedString: string = "";
     sortedNumber: any = 0;
-    // number1: string = "";
-    // number2: string = "";
-    // result: number = 0;
 
     ngOnInit() {
         // this.sortingInputNumber;
     }
-    sortingInputNumber() {
+
+
+    sort(values) {
+
+        for (var i = 0; i < values.length - 1; i++) {
+            var min = i;
+            for (var j = i + 1; j < values.length; j++) {
+                if (values[j] < values[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                var temp = values[i];
+                values[i] = values[min];
+                values[min] = temp;
+            }
+        }
+    }
+
+    splitSortingInputNumber() {
         // console.dir(this.sortedString.split(",").map(i => parseInt(i)));
         // if(this.sortedString = "string"){}
         this.sortedNumber = this.sortedString.split(",").map(i => parseInt(i))
+        console.log(this.sortedNumber); // split -method is used to split a string into an array of substrings, and returns the new array.
+        this.sort(this.sortedNumber);
+
+
         console.log(this.sortedNumber);
-        sorting(this.sortedNumber);
-            
-            function sorting(values){
-            //   alert(values);
-        
-                for (var i = 0; i < values.length - 1; i++){
-                    var min = i ;
-                    for(var j = i + 1; j < values.length; j++){
-                        if(values[j]< values[min]){
-                            min = j;
-                        }
-                    }
-                    if (min != i){
-                        var temp = values[i];
-                        values[i] = values[min];
-                        values[min] = temp;
-                    }
-                }
-                // alert(values);
-                // return this.sortedNumber;
-            }
-            console.log(this.sortedNumber);
-        // let inputtedString = this.sortedString;
-        // var inputtedString = "54321";
-        // console.log(inputtedString);
-        // this.sortedNumber = parseInt(this.sortedString);
-        // console.log(this.sortedNumber);
     }
-    
 
-    // calculate() {
-    //     this.result = parseInt(this.number1) + parseInt(this.number2);
-    //     console.log(this.result)
-    // }
-
-    // var numbers = [2,1,9,8,5,11,3,4,10,6,7];
-    
 }
